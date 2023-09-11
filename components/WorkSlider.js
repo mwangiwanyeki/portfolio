464bb4c261dff8ca909a9d2e87d23a4e5a1f94b1
mwangiwanyeki <37798128+mwangiwanyeki@users.noreply.github.com>
2023-09-11 13:08:27 +0300

    Update WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -35,7 +35,7 @@ const workSlides = {
         },
         {
           title: 'Creative Agency Landing Page',
-          path: '/proj44.png',
+          path: '/creative-agency.png',
           proj_url: 'https://creative-agency-landing-page.netlify.app/',
         },
       ],
